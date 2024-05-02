405b2a7e8a33cb4a93c03508a9b83fa37393a489
KimByeongHwa <iambyeonghwa@gmail.com>
2024-05-03 01:56:50 +0900

    회원가입 기능 구현(dto 설정 및 ValidationPipe 적용)

---
+++ src/auth/dto/auth-credential.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
+export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  // 숫자와 영어만 가능한 유효성 체크
+  @Matches(/^[a-zA-Z0-9]*S/, {
+    message: 'password only accepts number and english',
+  })
+  password: string;
+}
