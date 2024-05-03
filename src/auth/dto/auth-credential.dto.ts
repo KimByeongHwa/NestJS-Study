9ced0cdfc2cc312b49ae2676beb62c5edc79edb8
KimByeongHwa <iambyeonghwa@gmail.com>
2024-05-03 20:24:51 +0900

    정규식 오타 수정

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -10,7 +10,7 @@ export class AuthCredentialDto {
   @MinLength(4)
   @MaxLength(20)
   // 숫자와 영어만 가능한 유효성 체크
-  @Matches(/^[a-zA-Z0-9]*S/, {
+  @Matches(/^[a-zA-Z0-9]*$/, {
     message: 'password only accepts number and english',
   })
   password: string;
